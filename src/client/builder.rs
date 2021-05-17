PythonCreator27/use-github-api-rs

commit c72c651e152c9b22e8ffa0f017b81e38a30ec5d7

docs: fix docs and use doc(cfg)
Fix up some docs stuff and use doc(cfg) when on docs.rs.

src/client/builder.rs
@@ -16,6 +16,7 @@ use crate::GithubClient;
 /// // If `enterprise` is not enabled
 /// let client = GithubClientBuilder::new().auth("ghp_kajshdkja").build().unwrap();
 /// ```
+#[cfg_attr(docsrs, doc(cfg(feature = "auth")))]
 pub struct GithubClientBuilder<'a> {
     #[cfg(feature = "enterprise")]
     base_url: Option<&'a str>,
@@ -25,7 +26,6 @@ pub struct GithubClientBuilder<'a> {
 
 impl<'a> GithubClientBuilder<'a> {
     /// Creates a new `GithubClientBuilder`.
-
     pub fn new() -> Self {
         Self {
             #[cfg(feature = "auth")]
@@ -55,7 +55,7 @@ impl<'a> GithubClientBuilder<'a> {
         };
     }
 
-    #[cfg(feature = "auth")]
+    #[cfg(any(feature = "auth", doc))]
     /// Sets the auth token.
     /// # Examples
     /// ```rust
@@ -69,7 +69,7 @@ impl<'a> GithubClientBuilder<'a> {
         self
     }
 
-    #[cfg(feature = "enterprise")]
+    #[cfg(any(feature = "enterprise", doc))]
     /// Sets the base url.
     /// # Examples
     /// ```rust

Cargo.toml
@@ -25,3 +25,7 @@ default = ["auth"]
 node_ids = []
 auth = []
 enterprise = ["auth"] # Auth is required for enterprise GH
+
+[package.metadata.docs.rs]
+all-features = true
+rustdoc-args = ["--cfg", "docsrs"]

src/client/mod.rs
@@ -50,7 +50,7 @@ impl<'a> GithubClient<'a> {
     /// The base url can be like this: `https://somehostfor.github.enterprise.org/api/v3`.
     /// Do make sure to add the `https://` and the `/api/v3`.
     /// ## Auth Token
-    /// If using a PAT (personal access token), you can obtain one from https://github.com/settings/tokens.
+    /// If using a PAT (personal access token), you can obtain one from <https://github.com/settings/tokens>.
     /// # Errors
     /// Will error if the protocol is not `http://` or `https://`, and will also error if the base URL does not include `/api/v3`.
     /// Will also error if the reqwest client fails to build.

src/error.rs
@@ -1,4 +1,4 @@
-#[cfg(any(feature = "auth", feature = "enterprise"))]
+#[cfg(any(feature = "auth"))]
 pub mod creation {
     use std::{error::Error as StdError, fmt};
 

src/lib.rs
@@ -1,4 +1,5 @@
 #![warn(rust_2018_idioms)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 pub mod client;
 mod error;
 pub mod schema;

commit e6f6f1b80171cea9090032d06ded39579178f3d0

docs: fix doctest failure caused by c72c651e

src/client/builder.rs
@@ -75,6 +75,7 @@ impl<'a> GithubClientBuilder<'a> {
     /// ```rust
     /// # use use_github_api::GithubClientBuilder;
     /// let builder = GithubClientBuilder::new();
+    /// # #[cfg(feature = "enterprise")]
     /// let builder = builder.base_url("https://something.com/api/v3");
     /// // Build client and do stuff
     /// ```